7440159c81a0d3eadfed38cb7a91dd99bd409977
tautvydasvarnas1997 <tautvydas.varnas.97@gmail.com>
2024-12-09 16:12:41 +0200

    Buvo parašyti skriptai

---
+++ index.js
@@ -0,0 +1,4 @@
+// pirmas div
+
+document.getElementById('pirma-antraste').innerHTML = 'Naujas pirmos antrastes tekstas';
+document.getElementById('pirmas paragrafas').innerHTML = 'Naujas pakeistas pirmo paragrafo tekstas';
