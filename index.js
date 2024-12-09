499e1ac26e500c692f3d7a26f5f74b9007552738
tautvydasvarnas1997 <tautvydas.varnas.97@gmail.com>
2024-12-09 16:24:39 +0200

    Buvo parašytas antras div ir antro div skriptai

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 
 document.getElementById('pirma-antraste').innerHTML = 'Naujas pirmos antrastes tekstas';
 document.getElementById('pirmas paragrafas').innerHTML = 'Naujas pakeistas pirmo paragrafo tekstas';
+
+// antras div
+
+console.log('ANTRAS DIV');
+
+let tekstasIsParagrafo = document.getElementById('paragrafo-tekstas').innerHTML;
+console.log('paimtas su innerHTML tekstas:', tekstasIsParagrafo);
+
+let tekstasIsParagrafo2 = document.getElementById('paragrafo-tekstas').innerText;
+console.log('paimtas su innerText tekstas:', tekstasIsParagrafo2);
+
+console.log();
