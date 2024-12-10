c9c36f7d03e171f348c072d9f28a2bd5e9e3fd73
tautvydasvarnas1997 <tautvydas.varnas.97@gmail.com>
2024-12-10 14:57:51 +0200

    Buvo parašytas dešimtas div su skaičiavimais

--- index.js
+++ index.js
@@ -45,3 +45,18 @@ function pakeisti3(elementas, tekstas) {
 function kitaSpalva(elementas, spalva) {
     document.getElementById(elementas).style.color = spalva;
 }
+
+// desimtas div
+
+var pirmas = 8;
+var antras = 9;
+
+var suma = pirmas + antras;
+var skirtumas = pirmas - antras;
+var sandauga = pirmas * antras;
+var dalmuo = pirmas / antras;
+
+document.getElementById("suma").innerHTML = pirmas + " + " + antras + " = " + suma;
+document.getElementById("skirtumas").innerHTML = pirmas + " - " + antras + " = " + skirtumas;
+document.getElementById("sandauga").innerHTML = pirmas + " * " + antras + " = " + sandauga;
+document.getElementById("dalmuo").innerHTML = pirmas + " / " + antras + " = " + dalmuo;
